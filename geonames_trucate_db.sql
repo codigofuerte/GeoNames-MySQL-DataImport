34321eab4f79427477798287c5b3a3764d54dd91
scmoral <scmoral@codigofuerte.net>
2012-10-25 13:34:07 +0200

    Corrected syntax error

--- geonames_trucate_db.sql
+++ geonames_trucate_db.sql
@@ -1,10 +1,10 @@
 USE geonames;
-TRUNCATE TABLE geoname
-TRUNCATE TABLE alternatename
-TRUNCATE TABLE countryinfo
-TRUNCATE TABLE iso_languagecodes
-TRUNCATE TABLE admin1Codes
-TRUNCATE TABLE admin1CodesAscii
-TRUNCATE TABLE featureCodes
-TRUNCATE TABLE timeZones
-TRUNCATE TABLE continentCodes
+TRUNCATE TABLE geoname;
+TRUNCATE TABLE alternatename;
+TRUNCATE TABLE countryinfo;
+TRUNCATE TABLE iso_languagecodes;
+TRUNCATE TABLE admin1Codes;
+TRUNCATE TABLE admin1CodesAscii;
+TRUNCATE TABLE featureCodes;
+TRUNCATE TABLE timeZones;
+TRUNCATE TABLE continentCodes;
