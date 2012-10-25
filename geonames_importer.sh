4c0c6feef64fe44e81509758d7583bdbf71d8ae2
scmoral <scmoral@codigofuerte.net>
2012-10-25 12:46:12 +0200

    Incluída posibilidad de descargar datos de geonames Para realizar esta acción invocar el comando con la opción --download-data

--- geonames_importer.sh
+++ geonames_importer.sh
@@ -1,48 +1,80 @@
 #!/bin/bash
 
-EXPECTED_ARGS=3
+# Description and number of arguments used in this script
+expected_args=3
+
+#DOWNLOAD_GEONAMES_DATA_ACTION=  --download-data
+#CREATE_DATABASE_ACTION= 		--create-db
+#IMPORT_DUMPS_ACTION=			--import-dumps
+#DROP_DB_ACTION=					--drop-db
+#TRUNCATE_DB_ACTION=				--truncate-db
+
 
 logo() {
-	echo "==============================================="
-	echo " G E O N A M E S    D A T A    I M P O R T E R "
-	echo "==============================================="
+	echo "================================================================================================"
+	echo "                           G E O N A M E S    D A T A    I M P O R T E R                        "
+	echo "================================================================================================"
 }
 
-if [ $# -ne $EXPECTED_ARGS ]; then 
-	echo Usage: $0 "[--create-db | --import-dumps | --drop-db | --truncate-db]  <user> <password>"
-    echo "--create-db Creates the mysl database structure with no data."
-    echo "--import-dumps Imports geonames data into db. A database is previously needed for this to work."
-	echo "--drop-db Removes the db completely."
-    echo "--truncate-db Removes geonames data from db."
+usage() {
+	logo
+	echo Usage 1:  $0 --download-data "(this gets the current in GeoNames.org to the local machine)."
+	echo
+	echo Usage 2:  $0 "<action> <user> <password> (this is to operate with the database)"
+    echo Where action can be one of this:
+	echo "    --download-data Downloads the last packages of data available in GeoNames."
+    echo "    --create-db Creates the mysl database structure with no data."
+    echo "    --import-dumps Imports geonames data into db. A database is previously needed for this to work."
+	echo "    --drop-db Removes the db completely."
+    echo "    --truncate-db Removes geonames data from db."
+    echo
 	echo "<user> User name to access database server."
 	echo "<password> User password to access database server."
+	echo "================================================================================================"
     exit -1
+}
+
+logo
+
+if { [ $# == 1 ] && [ "$1" == "--download-data" ]; } then
+	echo "Downloading GeoNames.org data..." 
+	wget http://download.geonames.org/export/dump/allCountries.zip
+	wget http://download.geonames.org/export/dump/alternateNames.zip
+	#wget http://download.geonames.org/export/dump/iso-languagecodes.txt
+	wget http://download.geonames.org/export/dump/admin1CodesASCII.txt
+	wget http://download.geonames.org/export/dump/featureCodes_en.txt
+	wget http://download.geonames.org/export/dump/timeZones.txt
+	wget http://download.geonames.org/export/dump/countryInfo.txt
+	unzip allCountries.zip
+	unzip alternateNames.zip
+	rm allCountries.zip
+	rm alternateNames.zip
+else
+	case "$1" in
+
+		"--create-db")
+			echo "Creating \"geonames\" database"
+			mysql -u $2 -p$3 < geonames_db_struct.sql
+		;;
+
+		"--import-dumps")
+			echo "Importing \"geonames\" data into database"
+			mysql -u $2 -p$3 < geonames_db_struct.sql
+			mysql -u $2 -p$3 < geonames_import_data.sql
+		;;	
+
+		"--drop-db")
+			echo "Dropping \"geonames\" database"
+			mysql -u $2 -p$3 < geonames_drop_database.sql;
+		;;
+
+		"--truncate-db")
+			echo "Truncating \"geonames\" database"
+			mysql -u $2 -p$3 < geonames_trucate_db.sql;
+		;;
+
+	esac
 fi
-
-case "$1" in
-	"--create-db")
-		logo
-		echo "Creating \"geonames\" database"
-		mysql -u $2 -p$3 < geonames_db_struct.sql
-	;;
-	"--import-dumps")
-		logo
-		echo "Importing \"geonames\" data into database"
-		mysql -u $2 -p$3 < geonames_db_struct.sql
-		mysql -u $2 -p$3 < geonames_import_data.sql
-	;;	
-	"--drop-db")	
-		logo
-		echo "Dropping \"geonames\" database"
-		mysql -u $2 -p$3 < geonames_drop_database.sql;
-	;;
-	"--truncate-db")
-		logo
-		echo "Truncating \"geonames\" database"
-		mysql -u $2 -p$3 < geonames_trucate_db.sql;
-	;;
-
-esac
 
 if [ $? == 0 ]; then 
 	echo "[OK]"
