57d02f3c94b6facc739bc4abe8d1521285a14141
scmoral <scmoral@codigofuerte.net>
2012-10-29 17:32:48 +0100

    Code cleaning

--- geonames_importer.sh
+++ geonames_importer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-expected_args=3
+# Default values for database variables.
+dbhost="localhost"
+dbport=3306
+dbname="geonames"
 
 logo() {
 	echo "================================================================================================"
@@ -10,25 +13,29 @@ logo() {
 
 usage() {
 	logo
-	echo Usage 1:  $0 --download-data "(this gets the current in GeoNames.org to the local machine)."
+	echo "Usage 1: " $0 "--download-data"
+	echo "In this mode the current GeoNames.org's dumps are downloaded to the local machine."
 	echo
-	echo Usage 2:  $0 "<action> <user> <password> (this is to operate with the database)"
-    echo Where action can be one of this:
-	echo "    --download-data Downloads the last packages of data available in GeoNames."
-    echo "    --create-db Creates the mysl database structure with no data."
-    echo "    --import-dumps Imports geonames data into db. A database is previously needed for this to work."
-	echo "    --drop-db Removes the db completely."
-    echo "    --truncate-db Removes geonames data from db."
+	echo "Usage 2: " $0 "-a <action> -u <user> -p <password> -h <host> -r <port> -n <dbname>"
+	echo " This is to operate with the geographic database"
+    echo " Where <action> can be one of this: "
+	echo "    download-data Downloads the last packages of data available in GeoNames."
+    echo "    create-db Creates the mysl database structure with no data."
+    echo "    import-dumps Imports geonames data into db. A database is previously needed for this to work."
+	echo "    drop-db Removes the db completely."
+    echo "    truncate-db Removes geonames data from db."
     echo
-	echo "<user> User name to access database server."
-	echo "<password> User password to access database server."
+    echo " The rest of parameters indicates the following information:"
+	echo "    -u <user>     User name to access database server."
+	echo "    -p <password> User password to access database server."
+	echo "    -h <host>     Data Base Server address (default: localhost)."
+	echo "    -P <port>     Data Base Server Port (default: 3306)"
+	echo "    -n <dbname>  Data Base Name for the geonames.org data (default: geonames)"
 	echo "================================================================================================"
     exit -1
 }
 
-logo
-
-if { [ $# == 1 ] && [ "$1" == "--download-data" ]; } then
+download_geonames_data() {
 	echo "Downloading GeoNames.org data..." 
 	wget http://download.geonames.org/export/dump/allCountries.zip
 	wget http://download.geonames.org/export/dump/alternateNames.zip
@@ -40,10 +47,55 @@ if { [ $# == 1 ] && [ "$1" == "--download-data" ]; } then
 	unzip alternateNames.zip
 	rm allCountries.zip
 	rm alternateNames.zip
-else
-	case "$1" in
+}
 
-		"--create-db")
+if [ $# -lt 1 ]; then
+	usage
+	exit 1
+fi
+
+logo
+
+# Deals operation mode 1 (Download data bundles from geonames.org)
+if { [ $# == 1 ] && [ "$1" == "--download-data" ]; } then
+    download_geonames_data
+	exit 0
+fi
+
+# Deals with operation mode 2 (Database issues...)
+# Parses command line parameters.
+while getopts "a:u:p:h:r:n:" opt; 
+do
+    case $opt in
+        a) action=$OPTARG ;;
+        u) dbusername=$OPTARG ;;
+        p) dbpassword=$OPTARG ;;
+        h) dbhost=$OPTARG ;;
+        r) dbport=$OPTARG ;;
+        n) dbname=$OPTARG ;;
+    esac
+done
+
+#echo "Orden: " $action
+#echo "UserName: " $dbusername
+#echo "Password: " $dbpassword
+#echo "DB Host: " $dbhost
+#echo "DB Port: " $dbport
+#echo "DB Name: " $dbname
+#exit 0
+
+case $action in
+    download-data)
+        download_geonames_data
+        exit 0
+    ;;
+esac
+
+while [ $# -gt 1 ]; do
+    case "$1" in
+
+        --create-db)
+            
 			echo "Creating \"geonames\" database"
 			mysql -u $2 -p$3 < geonames_db_struct.sql
 		;;
@@ -65,7 +117,8 @@ else
 		;;
 
 	esac
-fi
+done
+
 
 if [ $? == 0 ]; then 
 	echo "[OK]"
