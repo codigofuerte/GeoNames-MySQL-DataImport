01d6d644d63a2638f421911b48446efb60cd33f3
Sergio Cruz <scmoral@codigofuerte.net>
2017-08-23 18:07:53 +0200

    Primera aproximación a la nueva disposición de directorios de descarga y ejecución de la aplicación.

--- geonames_importer.sh
+++ geonames_importer.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# [BEGIN] CONFIGURATION FOR THE SCRIPT
+# -------------------------------------
 # Default values for database variables.
 dbhost="localhost"
 dbport=3306
 dbname="geonames"
-#dir=$( cd "$( dirname "$0" )" && pwd )
+dbusername= "root"
+dbpassword= "root"
 
-#download_folder="`pwd`/download"
-download_folder="`pwd`"
+# Default value for download folder
+download_folder="download"
+
+# Default general dumps to download
+dumps= "allCountries.zip alternateNames.zip hierarchy.zip admin1CodesASCII.txt admin2Codes.txt featureCodes_en.txt timeZones.txt countryInfo.txt"
+# By default all postal codes ... You can specify a set of the files located at http://download.geonames.org/export/zip/
+postal_codes= "allCountries.zip"
+
+# [END] CONFIGURATION FOR THE SCRIPT
+# -------------------------------------
 
 logo() {
 echo " __________________________________________________________________________________________________________________ "
@@ -18,29 +29,37 @@ echo " | | |_ |/ _ \/ _ \| '_ \ / _` | '_ ` _ \ / _ \/ __| | |  | |/ _` | __/ _`
 echo " | |__| |  __/ (_) | | | | (_| | | | | | |  __/\__ \ | |__| | (_| | || (_| |  _| |_| | | | | | |_) | (_) | |  | |_  "
 echo "  \_____|\___|\___/|_| |_|\__,_|_| |_| |_|\___||___/ |_____/ \__,_|\__\__,_| |_____|_| |_| |_| .__/ \___/|_|   \__| "
 echo "                                                                                             |_|                    "
-echo " __________________________________________________________________________________________________________________ "
+echo " __________________________________________________ v 2.0 _________________________________________________________ "
 }
 
 usage() {
 	logo
-	echo "Usage: " $0 "-a <action> -u <user> -p <password> -h <host> -r <port> -n <dbname>"
-	echo " This is to operate with the geographic database"
+	echo "Usage: " $0 "-a <action> "
     echo " Where <action> can be one of this: "
-	echo "    download-data Downloads the last packages of data available in GeoNames. An additional parameter with a download directory should be used."
-    echo "    create-db Creates the mysql database structure with no data."
-    echo "    create-tables Creates the tables in the current database. Useful if we want to import them in an exsiting db."
-    echo "    import-dumps Imports geonames data into db. A database is previously needed for this to work."
-	echo "    drop-db Removes the db completely."
-    echo "    truncate-db Removes geonames data from db."
+	echo "    download-data: Downloads the last packages of data available in GeoNames. An additional parameter with a download directory should be used."
+    echo "    create-db: Creates the mysql database structure with no data."
+    echo "    create-tables: Creates the tables in the current database. Useful if we want to import them in an exsiting db."
+    echo "    import-dumps: Imports geonames data into db. A database is previously needed for this to work."
+	echo "    drop-db: Removes the db completely."
+    echo "    truncate-db: Removes geonames data from db."
     echo
-    echo " The rest of parameters indicates the following information:"
-	echo "    -u <user>     User name to access database server."
-	echo "    -p <password> User password to access database server."
-	echo "    -h <host>     Data Base Server address (default: localhost)."
-	echo "    -r <port>     Data Base Server Port (default: 3306)"
-	echo "    -n <dbname>  Data Base Name for the geonames.org data (default: geonames)"
-	echo "================================================================================================"
+    # echo " The rest of parameters indicates the following information:"
+	# echo "    -u <user>     User name to access database server."
+	# echo "    -p <password> User password to access database server."
+	# echo "    -h <host>     Data Base Server address (default: localhost)."
+	# echo "    -r <port>     Data Base Server Port (default: 3306)"
+	# echo "    -n <dbname>  Data Base Name for the geonames.org data (default: geonames)"
     exit -1
+}
+
+dump_db_params() {
+    echo "Database parameters being used..."
+    echo "Orden: " $action
+    echo "UserName: " $dbusername
+    echo "Password: " $dbpassword
+    echo "DB Host: " $dbhost
+    echo "DB Port: " $dbport
+    echo "DB Name: " $dbname
 }
 
 # download_geonames_data() {
@@ -97,44 +116,60 @@ done
 
 case $action in
     download-data)
+        echo "STARTING DATA DOWNLOAD !!"
+        # Checks if a download folder has been specified.
         if { [ "$3" != "" ]; } then
             if [ ! -d "$3" ]; then
-                echo "Temporary download data folder '$2' doesn't exists. Creating it."
-                mkdir "$3"
+                echo "Temporary download data folder '$3' doesn't exists. Creating it."
+                mkdir -p "$3"
             fi
-            download_folder="`pwd`/$2"
+            download_folder="`pwd`/$3"
+        else
+            if [ ! -d "$download_folder" ]; then
+                echo "Temporary download data folder '$download_folder' doesn't exists. Creating it."
+                mkdir -p "$download_folder"
+            fi
         fi
-        # Dumps general Data
-        dumps="allCountries.zip alternateNames.zip hierarchy.zip admin1CodesASCII.txt admin2Codes.txt featureCodes_en.txt timeZones.txt countryInfo.txt"
+
+        # Dumps General data.
+        echo "Downloading general files"
         for dump in $dumps; do
+            echo "Downloading $dump into $download_folder"
             wget -c -P "$download_folder" http://download.geonames.org/export/dump/$dump
             if [ ${dump: -4} == ".zip" ]; then
-                echo "Unzipping $dump..."
+                echo "Unzipping $dump into `pwd`"
                 unzip "$download_folder/$dump" -d "`pwd`"
             fi
         done
-        # TODO: Dumps Postal Code Data.
+
+        # Dumps Postal Code data.
+        echo "Downloading postal code information"
+        if [ ! -d "$download_folder/zip_codes" ]; then
+                echo "Temporary download data folder '$download_folder/zip_codes' doesn't exists. Creating it."
+                mkdir -p "$download_folder/zip_codes"
+            fi        
+        for postal_code_file in $postal_codes; do
+            echo "Downloading $postal_code_file into $download_folder/zip_codes"
+            wget -c -P "$download_folder/zip_codes" http://download.geonames.org/export/zip/$postal_code_file
+            if [ ${dump: -4} == ".zip" ]; then
+                echo "Unzipping Postal Code file $postal_code_file into $download_folder/zip_codes"
+                unzip "$download_folder/zip_codes/$postal_code_file" -d "$download_folder/zip_codes"
+            fi 
+        done
+        echo "DATA DOWNLOAD FINISH !!"
         exit 0
     ;;
 esac
 
-if [ -z $dbusername ]; then
-    echo "No user name provided for accessing the database. Please write some value in parameter -u..."
-    exit 1
-fi
+# if [ -z $dbusername ]; then
+#     echo "No user name provided for accessing the database. Please write some value in parameter -u..."
+#     exit 1
+# fi
 
-if [ -z $dbpassword ]; then
-    echo "No user password provided for accessing the database. Please write some value in parameter -p..."
-    exit 1
-fi
-
-echo "Database parameters being used..."
-echo "Orden: " $action
-echo "UserName: " $dbusername
-echo "Password: " $dbpassword
-echo "DB Host: " $dbhost
-echo "DB Port: " $dbport
-echo "DB Name: " $dbname
+# if [ -z $dbpassword ]; then
+#     echo "No user password provided for accessing the database. Please write some value in parameter -p..."
+#     exit 1
+# fi
 
 case "$action" in
     create-db)
@@ -142,18 +177,18 @@ case "$action" in
         mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword -Bse "DROP DATABASE IF EXISTS $dbname;"
         mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword -Bse "CREATE DATABASE $dbname DEFAULT CHARACTER SET utf8;" 
         mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword -Bse "USE $dbname;" 
-        mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword $dbname < $dir/geonames_db_struct.sql
+        mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword $dbname < `pwd`/geonames_db_struct.sql
     ;;
         
     create-tables)
         echo "Creating tables for database $dbname..."
         mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword -Bse "USE $dbname;" 
-        mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword $dbname < $dir/geonames_db_struct.sql
+        mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword $dbname < `pwd`/geonames_db_struct.sql
     ;;
     
     import-dumps)
         echo "Importing geonames dumps into database $dbname"
-        mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword --local-infile=1 $dbname < $dir/geonames_import_data.sql
+        mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword --local-infile=1 $dbname < `pwd`/geonames_import_data.sql
     ;;    
     
     drop-db)
@@ -163,7 +198,7 @@ case "$action" in
         
     truncate-db)
         echo "Truncating \"geonames\" database"
-        mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword $dbname < $dir/geonames_truncate_db.sql
+        mysql -h $dbhost -P $dbport -u $dbusername -p$dbpassword $dbname < `pwd`/geonames_truncate_db.sql
     ;;
 esac
 
